6275e4f514f8890666ca6c3eca6b8bee11e8161d
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-11 19:52:06 +0800

    added sample controller and tests

---
+++ src/main/java/com/userinfo_service/controller/UserInfoController.java
@@ -0,0 +1,16 @@
+package com.userinfo_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-info")
+public class UserInfoController {
+
+    @GetMapping()
+    public ResponseEntity<?> getUserInfo() {
+        return  ResponseEntity.ok("Jovanie");
+    }
+}
